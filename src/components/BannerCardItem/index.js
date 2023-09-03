fbc437daa3d961dd2d665de7f040845884353d9c
zackdaspy <zahedajafari@gmail.com>
2023-09-03 06:07:20 +0000

    first commit

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const BannerCardItem = ({id, headerText, description, className}) => (
+  <li className={className} key={id}>
+    <h2>{headerText}</h2>
+    <p>{description}</p>
+    <button className="show-more-button" type="button">
+      Show More
+    </button>
+  </li>
+)
+
+export default BannerCardItem
